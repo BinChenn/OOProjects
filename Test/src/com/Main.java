4eba3f718bfa4afa6dcaac0b1c65caadbf5db095
BinChenn <binchinsmail@gmail.com>
2018-09-21 15:39:28 +0800

    first

---
+++ Test/src/com/Main.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World, this is my first GitHub project!");
+
+	// write your code here
+    }
+}
