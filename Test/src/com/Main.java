6941c10418fae2370cf32cd9f7bf00eadd291a1b
BinChenn <binchinsmail@gmail.com>
2018-09-22 17:18:51 +0800

    add class Pad

--- Test/src/com/Main.java
+++ Test/src/com/Main.java
@@ -4,7 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World, this is my first GitHub project!");
+        System.out.println("Hello, this is the code I typed in my CUBE Pad");
+        
+        Pad cube3x = new Pad(1, "iwork");
+        System.out.println("cube3x: " + cube3x.getPadName());
+
+
 
 	// write your code here
     }
 }
+
