f2ce31053168012ba33480006e3ba19cc56143f8
BinChenn <binchinsmail@gmail.com>
2018-09-30 10:51:43 +0800

    add car

--- CarSelling/src/com/Car.java
+++ CarSelling/src/com/Car.java
@@ -3,8 +3,128 @@ package com;
 import java.util.Date;
 
 public abstract class Car {
-    private String name;
+    private Double price;
+    private int quanity;
     private Date InDate;
     private Date OutDate;
 
+    public Date getInDate() {
+        return InDate;
+    }
+    public void setInDate(Date inDate) {
+        InDate = inDate;
+    }
+
+    public Date getOutDate() {
+        return OutDate;
+    }
+
+    public void setOutDate(Date outDate) {
+        OutDate = outDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQuanity() {
+        return quanity;
+    }
+
+    public void setQuanity(int quanity) {
+        this.quanity = quanity;
+    }
+
+    public abstract Double getMoney();
+    public abstract void InCar();
+    public abstract void OutCar();
+}
+
+class ToyataCar extends Car{
+    @Override
+    public Double getMoney() {
+        return getPrice()*getQuanity();
+    }
+
+    @Override
+    public void InCar() {
+
+    }
+
+    @Override
+    public void OutCar() {
+
+    }
+}
+
+class BMWCar extends Car{
+    @Override
+    public Double getMoney() {
+        return getPrice()*getQuanity();
+    }
+
+    @Override
+    public void InCar() {
+
+    }
+
+    @Override
+    public void OutCar() {
+
+    }
+}
+
+class FordCar extends Car{
+    @Override
+    public Double getMoney() {
+        return getPrice()*getQuanity();
+    }
+
+    @Override
+    public void InCar() {
+
+    }
+
+    @Override
+    public void OutCar() {
+
+    }
+}
+
+class BenzCar extends Car{
+    @Override
+    public Double getMoney() {
+        return getPrice()*getQuanity();
+    }
+
+    @Override
+    public void InCar() {
+
+    }
+
+    @Override
+    public void OutCar() {
+
+    }
+}
+
+class AudiCar extends Car{
+    @Override
+    public Double getMoney() {
+        return getPrice()*getQuanity();
+    }
+
+    @Override
+    public void InCar() {
+
+    }
+
+    @Override
+    public void OutCar() {
+
+    }
 }
---
+++ CarSelling/src/com/CarFactory.java
@@ -0,0 +1,22 @@
+package com;
+
+public class CarFactory {
+    public static Car createCar(String name)throws Exception{
+        if(name.equalsIgnoreCase("Toyota"))
+            return new ToyataCar();
+        else if(name.equalsIgnoreCase("Ford"))
+            return new FordCar();
+        else if(name.equalsIgnoreCase("Benz"))
+            return new BenzCar();
+        else if(name.equalsIgnoreCase("Audi"))
+            return new AudiCar();
+        else if(name.equalsIgnoreCase("BWM"))
+            return new BMWCar();
+        else
+            throw new Exception();
+
+
+
+    }
+
+}
